f8377de5224ddf83d0a3a09b22fb8b85cc886bab
MadushanR <madushanrajendran@gmail.com>
2025-03-05 14:14:31 -0500

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,18 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import ForgotPassword from "./components/ForgotPassword"; 
 import PlaceOrder from "./components/PlaceOrder";
-
+import ManagerDashboard from './Manager/ManagerDashboard';
+import AddItem from "./Manager/AddItem";
+import Navbar from './Navbar';
 //const Home = () => <h1>Welcome to Restaurant Management System</h1>; // Temporary Home Page
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
+        <Route path="/Manager/ManagerDashboard" element={<ManagerDashboard />} />
+        <Route path="/additem" element={<AddItem />} />
         <Route path="/" element={<Home />} />   {/* ✅ Add this route */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
